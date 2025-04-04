53de47dc70b4ba7b741bc6f5b924b6592fe67e2c
foxdog <alexkolymago@gmail.com>
2025-04-04 17:56:24 +0300

    refactor: continue changing structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,39 +1,4 @@
 #!/usr/bin/env node
-import { askName, sayHello } from '../src/cli.js';
-import getRightAnswer from '../src/games/even.js';
-import {
-  welcomeUser,
-  explainRule,
-  getRandomNumber,
-  askQuestion,
-  getAnswer,
-  isUserRight,
-  sayCorrect,
-  sayWrong,
-  congratulateUser,
-} from '../../../../../src/index.js';
+import playGame from '../src/games/even.js';
 
-const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-let roundsCount = 3;
-let hasWon;
-welcomeUser();
-const usersName = askName();
-sayHello(usersName);
-explainRule(rule);
-while (roundsCount > 0) {
-  const randomNumber = getRandomNumber();
-  askQuestion(`Question: ${randomNumber}`);
-  const usersAnswer = getAnswer();
-  const rightAnswer = getRightAnswer(randomNumber);
-  hasWon = isUserRight(usersAnswer, rightAnswer);
-  if (hasWon) {
-    sayCorrect();
-    roundsCount -= 1;
-  } else {
-    sayWrong(usersAnswer, rightAnswer, usersName);
-    roundsCount = 0;
-  }
-}
-if (hasWon) {
-  congratulateUser(usersName);
-}
+playGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,3 +1,17 @@
+import { askName, sayHello } from '../cli.js';
+
+import {
+  welcomeUser,
+  explainRule,
+  getRandomNumber,
+  askQuestion,
+  getAnswer,
+  isUserRight,
+  sayCorrect,
+  sayWrong,
+  congratulateUser,
+} from '../index.js';
+
 function isEven(number) {
   if (number % 2 === 0) {
     return true;
@@ -10,4 +24,31 @@ function getRightAnswer(number) {
   return rightAnswer;
 }
 
-export default getRightAnswer;
+function playGame() {
+  const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+  let roundsCount = 3;
+  let hasWon;
+  welcomeUser();
+  const usersName = askName();
+  sayHello(usersName);
+  explainRule(rule);
+  while (roundsCount > 0) {
+    const randomNumber = getRandomNumber();
+    askQuestion(`Question: ${randomNumber}`);
+    const usersAnswer = getAnswer();
+    const rightAnswer = getRightAnswer(randomNumber);
+    hasWon = isUserRight(usersAnswer, rightAnswer);
+    if (hasWon) {
+      sayCorrect();
+      roundsCount -= 1;
+    } else {
+      sayWrong(usersAnswer, rightAnswer, usersName);
+      roundsCount = 0;
+    }
+  }
+  if (hasWon) {
+    congratulateUser(usersName);
+  }
+}
+
+export default playGame;
